779e1ed4e03f029e629275f18b8c59a6ce97d969
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-03-04 01:28:36 +0530

    added SessionFactory for session

---
+++ src/main/java/org/consoleadmin/Client.java
@@ -0,0 +1,40 @@
+package org.consoleadmin;
+
+public class Client {
+    private int empId;
+    private String empName;
+    private int empAge;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public int getEmpAge() {
+        return empAge;
+    }
+
+    public void setEmpAge(int empAge) {
+        this.empAge = empAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", empAge=" + empAge +
+                '}';
+    }
+}
