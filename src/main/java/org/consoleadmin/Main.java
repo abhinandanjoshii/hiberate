605d4e1e8588c2bbf301206216559e3c3ee5e419
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-03-05 00:11:51 +0530

    few adjustments

--- src/main/java/org/consoleadmin/Main.java
+++ src/main/java/org/consoleadmin/Main.java
@@ -9,20 +9,28 @@ public class Main {
     public static void main(String[] args) {
 
         Client c = new Client();
-        c.setEmpAge(21);
-        c.setEmpName("consoleAdmin");
-        c.setEmpId(12);
+        c.setEmpAge(1);
+        c.setEmpName("consoleAdminFive");
+        c.setEmpId(110);
 
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(org.consoleadmin.Client.class);
-        cfg.configure();
-        SessionFactory sf = cfg.buildSessionFactory();
+        Client c2 = null;
+
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(org.consoleadmin.Client.class);
+//        cfg.configure();
+
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(org.consoleadmin.Client.class)
+                .configure()
+                .buildSessionFactory();
         Session session = sf.openSession();
 
         Transaction transaction = session.beginTransaction();
-        session.save(c);
+        session.persist(c); //using newer version
         transaction.commit();
+        session.close();
+        sf.close();
 
-
+        System.out.println(c2);
     }
 }
