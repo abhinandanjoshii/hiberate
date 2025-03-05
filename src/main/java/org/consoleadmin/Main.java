c4050f07f5b0757c73f4f23f022047b3da7dd2e3
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-03-05 14:28:31 +0530

    upserting data

--- src/main/java/org/consoleadmin/Main.java
+++ src/main/java/org/consoleadmin/Main.java
@@ -2,28 +2,33 @@ package org.consoleadmin;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
 
         Client c = new Client();
-        c.setEmpAge(1);
-        c.setEmpName("consoleAdminFive");
-        c.setEmpId(110);
+        c.setEmpAge(23);
+        c.setEmpName("consoleAdminSix");
+        c.setEmpId(178);
 
-        Client c2 = null;
 
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(org.consoleadmin.Client.class)
                 .configure()
                 .buildSessionFactory();
         Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
 
-        c2 = session.get(Client.class,102);
+        // updating data if present, else create
+        session.merge(c);
+
+        transaction.commit();
+
         session.close();
         sf.close();
 
-        System.out.println(c2);
+        System.out.println(c);
     }
 }
